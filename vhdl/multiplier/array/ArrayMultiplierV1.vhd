00ec4f22269638f7b95172db919c36e837fd1d4d
mehrshad-kh <mjii@protonmail.com>
2024-05-01 02:03:10 +0330

    style: update ArrayMultiplierV1

--- vhdl/multiplier/array/ArrayMultiplierV1.vhd
+++ vhdl/multiplier/array/ArrayMultiplierV1.vhd
@@ -74,64 +74,64 @@ begin
         cin => carries(0)(1));
         
     ha03: HalfAdder port map (
-            sum => sums(0)(3),
-            cout => carries(0)(3),
-            a => products(3)(1),
-            b => carries(0)(2));
+        sum => sums(0)(3),
+        cout => carries(0)(3),
+        a => products(3)(1),
+        b => carries(0)(2));
             
     ha10: HalfAdder port map (
-          sum => sums(1)(0),
-          cout => carries(1)(0),
-          a => sums(0)(1),
-          b => products(0)(2));
+        sum => sums(1)(0),
+        cout => carries(1)(0),
+        a => sums(0)(1),
+        b => products(0)(2));
      
     fa11: FullAdder port map (
-                  sum => sums(1)(1),
-                  cout => carries(1)(1),
-                  a => sums(0)(2),
-                  b => products(1)(2),
-                  cin => carries(1)(0));
+        sum => sums(1)(1),
+        cout => carries(1)(1),
+        a => sums(0)(2),
+        b => products(1)(2),
+        cin => carries(1)(0));
                   
-     fa12: FullAdder port map (
-         sum => sums(1)(2),
-         cout => carries(1)(2),
-         a => sums(0)(3),
-         b => products(2)(2),
-         cin => carries(1)(1));
+    fa12: FullAdder port map (
+        sum => sums(1)(2),
+        cout => carries(1)(2),
+        a => sums(0)(3),
+        b => products(2)(2),
+        cin => carries(1)(1));
          
-     fa13: FullAdder port map (
-                  sum => sums(1)(3),
-                  cout => carries(1)(3),
-                  a => carries(0)(3),
-                  b => products(3)(2),
-                  cin => carries(1)(2));   
-                  
-      ha20: HalfAdder port map (
-          sum => sums(2)(0),
-          cout => carries(2)(0),
-          a => sums(1)(1),
-          b => products(0)(3));
+    fa13: FullAdder port map (
+        sum => sums(1)(3),
+        cout => carries(1)(3),
+        a => carries(0)(3),
+        b => products(3)(2),
+        cin => carries(1)(2));   
+      
+    ha20: HalfAdder port map (
+        sum => sums(2)(0),
+        cout => carries(2)(0),
+        a => sums(1)(1),
+        b => products(0)(3));
           
-        fa21: FullAdder port map (
-              sum => sums(2)(1),
-              cout => carries(2)(1),
-              a => sums(1)(2),
-              b => products(1)(3),
-              cin => carries(2)(0));
+    fa21: FullAdder port map (
+        sum => sums(2)(1),
+        cout => carries(2)(1),
+        a => sums(1)(2),
+        b => products(1)(3),
+        cin => carries(2)(0));
               
-        fa22: FullAdder port map (
-            sum => sums(2)(2),
-            cout => carries(2)(2),
-            a => sums(1)(3),
-            b => products(2)(3),
-            cin => carries(2)(1));       
-            
-        fa23: FullAdder port map (
-             sum => sums(2)(3),
-             cout => carries(2)(3),
-             a => carries(1)(3),
-             b => products(3)(3),
-             cin => carries(2)(2));         
+    fa22: FullAdder port map (
+        sum => sums(2)(2),
+        cout => carries(2)(2),
+        a => sums(1)(3),
+        b => products(2)(3),
+        cin => carries(2)(1));       
+           
+    fa23: FullAdder port map (
+        sum => sums(2)(3),
+        cout => carries(2)(3),
+        a => carries(1)(3),
+        b => products(3)(3),
+        cin => carries(2)(2));         
     
     output(0) <= products(0)(0);
     output(1) <= sums(0)(0);
